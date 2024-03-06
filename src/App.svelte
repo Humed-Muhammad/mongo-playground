15dcc3faf8f420f3bd44702d61bb6044e03884d0
Humed-Muhammad <abc45171@gmail.com>
2024-03-06 21:16:41 +0300

    using svelte

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+  // import type { Settings } from "./types";
+  import Editor from "./components/Editor.svelte";
+
+  let settings = JSON.parse(localStorage.getItem("mongodbSettings")) ?? {
+    url: "mongodb://localhost:27017",
+    theme: "vs-dark",
+    dbName: "test",
+    collectionName: "orders",
+    query: "[]",
+  };
+  let mongodbSettings;
+
+  const handleSettingChange = (newSettings) => {
+    settings = newSettings;
+  };
+
+  $: {
+    mongodbSettings = JSON.parse(
+      localStorage.getItem("mongodbSettings") || JSON.stringify(settings)
+    );
+  }
+
+  $: {
+    localStorage.setItem("mongodbSettings", JSON.stringify(settings));
+  }
+</script>
+
+<Editor {settings} {handleSettingChange} />
